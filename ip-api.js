db056388925100f4c17acf2f629efdd32648bbb1
haha <125535033+wangba2023@users.noreply.github.com>
2025-05-20 10:00:26 +0800

    Update ip-api.js

--- ip-api.js
+++ ip-api.js
@@ -43,8 +43,8 @@ var flags = new Map([["AC","🇦🇨"],["AD","🇦🇩"],["AE","🇦🇪"],["AF"
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title =flags.get(obj['countryCode']) +Area_check(obj['country']);
-var subtitle = obj['query'] +'❣️'+ obj['currency']+'❣️'+ obj['as']）
+var title =flags.get(obj['countryCode']) +Area_check(obj['country'])+" ❣️ "+obj['city'];
+var subtitle = obj['query'] +" ❣️ "+ obj['currency']+" ❣️ "+ obj['as']）
 var ip = obj['query']+flags.get(obj['countryCode']);
 var description = '------------------------------'+'\n'+'\n'+'🖥️服务商:'+obj['isp'] + '\n'+'\n'+'🌍地区:' +City_ValidCheck(obj['regionName'])+ '\n'+ '\n' + '🗺️IP地址:'+ obj['query'] + flags.get(obj['countryCode']) +'\n'+ '\n' +'🕗时区:'+ obj['timezone']+'\n'+'\n'+'📍经纬度：'+obj['lon']+ ','+obj['lat']+'\n' +'\n'+'🪙货币:'+ obj['currency'] +flags.get(obj['currency']);
 $done({title, subtitle,ip, description});
