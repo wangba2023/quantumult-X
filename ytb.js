462059e9cdf69f0670f1173093ef27d504b54ba3
haha <125535033+wangba2023@users.noreply.github.com>
2025-06-10 00:46:45 +0800

    更新 ytb.js

--- ytb.js
+++ ytb.js
@@ -1,15 +1,34 @@
-(() => {
-  const parse = JSON.parse($response.body);
-  const pr = parse.playerResponse || {};
-  
-  // 移除广告相关字段
-  ['adPlacements', 'playerAds', 'adSlots'].forEach(k => delete pr[k]);
-  
-  // 解锁区域限制
-  if (pr.playabilityStatus?.status === 'LOGIN_REQUIRED') {
-    pr.playabilityStatus.status = 'OK';
-    pr.playabilityStatus.messages = [];
-  }
-  
-  $done({ body: JSON.stringify(parse) });
-})();
+// 修改 G 类的 save 方法，添加重置标志的语句
+save() {
+    if (this.needSave) {
+        w.debug("Update Config");
+        let e = {
+            version: this.version,
+            whiteNo: this.whiteNo,
+            blackNo: this.blackNo,
+            whiteEml: this.whiteEml,
+            blackEml: this.blackEml
+        };
+        w.debug(e);
+        w.setJSON(e, "YouTubeAdvertiseInfo");
+        this.needSave = false; // 添加重置标志
+    }
+}
+
+// 修改 Ve 类的 pure 方法，添加子处理器的保存调用
+async pure() {
+    for (let e of this.message.contents) {
+        if (e.player) {
+            this.player.message = e.player;
+            await this.player.pure();
+            this.player.save(); // 保存 player 配置
+        }
+        if (e.next) {
+            this.next.message = e.next;
+            await this.next.pure();
+            this.next.save(); // 保存 next 配置
+        }
+        this.needProcess = true;
+    }
+    return this;
+}
